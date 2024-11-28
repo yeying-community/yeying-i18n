14a8e862687b66033c2bcd3ac2da3dc9e0cc817e
kobor <kobofare@gmail.com>
2024-11-28 08:33:25 +0800

    add localstorage

--- src/index.js
+++ src/index.js
@@ -26,23 +26,33 @@ resources[Languages.ZH_CN] = {
 
 i18next.use(LanguageDetector).init({
   resources,
-  lng: Languages.ZH_CN,
-  fallbackLng: [Languages.ZH_CN, Languages.EN_US],
+  detection: {
+    // 优先检测返回的语言缓存顺序
+    order: ['localStorage', 'navigator'],
+    caches: ['localStorage']
+  },
+  fallbackLng: Languages.EN_US,
   interpolation: {
     escapeValue: false, // react 已经保护了 XSS
   },
 }, (err, t) => {
   if (err) return console.log('something went wrong loading', err)
-  console.log('Successfully.', t('common.welcome'))
-})
+  console.log(t('common.welcome'))
+}).then(r => console.log('Successfully.', r))
 
 export function t(key) {
   return i18next.t(key)
 }
 
 export function setLanguage(lng) {
-  return i18next.changeLanguage(lng, (err, t) => {
-    if (err) return console.error(`Fail to language: ${lng}`, err)
-    console.log('current language:', lng)
-  })
+  const currentLang = i18next.language
+  if (currentLang !== lng) {
+    return i18next.changeLanguage(lng, (err, t) => {
+      if (err) return console.error(`Fail to language: ${lng}`, err)
+      console.log('current language:', lng)
+      localStorage.setItem('i18nextLng', lng); // 手动缓存语言到localStorage
+    })
+  } else {
+    console.log('Language is already set to:', lng)
+  }
 }
